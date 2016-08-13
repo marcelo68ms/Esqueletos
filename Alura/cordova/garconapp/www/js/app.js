51e94cb9c7783fb868d060646965d66ad2a779bd
Marcelo Santos <marcelo68ms@gmail.com>
2016-08-13 14:03:49 -0300

    Inicio dos exercícios #5

---
+++ Alura/cordova/garconapp/www/js/app.js
@@ -0,0 +1,15 @@
+$('.collection')
+    .on('click', '.collection-item', function(){
+
+        var nomeProduto = this.firstChild.textContent;
+        Materialize.toast(nomeProduto + ' adicionado', 1000);
+
+        var $badge = $('.badge', this);
+        if ($badge.length === 0) {
+            $badge = $('<span class="badge brown-text">0</span>')
+                        .appendTo(this);
+        }
+
+        $badge.text(parseInt($badge.text()) + 1);
+    })
+
