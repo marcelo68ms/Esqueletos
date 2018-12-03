a231d1fb803ba013394737f54fe588ea53823852
Marcelo - Dell <marcelo68ms@gmail.com>
2018-12-03 17:17:39 -0200

    Ajustes no endpoint e inclusão das exceções

--- Spring/cursomc/src/main/java/com/topseed/cursomc/services/CategoriaService.java
+++ Spring/cursomc/src/main/java/com/topseed/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.topseed.cursomc.domain.Categoria;
 import com.topseed.cursomc.repositories.CategoriaRepository;
+import com.topseed.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = rep.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
  	}
 }
