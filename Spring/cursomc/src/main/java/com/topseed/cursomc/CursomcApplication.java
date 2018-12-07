7315090157f556639de3adaf48e6918fcdf95461
Marcelo - Dell <marcelo68ms@gmail.com>
2018-12-07 14:45:13 -0200

    Finalização do Item de Pedido com sua inserção no banco.

--- Spring/cursomc/src/main/java/com/topseed/cursomc/CursomcApplication.java
+++ Spring/cursomc/src/main/java/com/topseed/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.topseed.cursomc.domain.Cidade;
 import com.topseed.cursomc.domain.Cliente;
 import com.topseed.cursomc.domain.Endereco;
 import com.topseed.cursomc.domain.Estado;
+import com.topseed.cursomc.domain.ItemPedido;
 import com.topseed.cursomc.domain.Pagamento;
 import com.topseed.cursomc.domain.PagamentoComBoleto;
 import com.topseed.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.topseed.cursomc.repositories.CidadeRepository;
 import com.topseed.cursomc.repositories.ClienteRepository;
 import com.topseed.cursomc.repositories.EnderecoRepository;
 import com.topseed.cursomc.repositories.EstadoRepository;
+import com.topseed.cursomc.repositories.ItemPedidoRepository;
 import com.topseed.cursomc.repositories.PagamentoRepository;
 import com.topseed.cursomc.repositories.PedidoRepository;
 import com.topseed.cursomc.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -118,5 +123,17 @@ public class CursomcApplication implements CommandLineRunner {
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
 	}
 }
--- Spring/cursomc/src/main/java/com/topseed/cursomc/domain/ItemPedido.java
+++ Spring/cursomc/src/main/java/com/topseed/cursomc/domain/ItemPedido.java
@@ -2,9 +2,14 @@ package com.topseed.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+@Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 	
 	private Double desconto;
@@ -12,8 +17,7 @@ public class ItemPedido implements Serializable {
 	private Double preco;
 	
 	public ItemPedido() {
-		super();
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public ItemPedido(Pedido pedido,Produto produto, Double desconto, Integer quantidade, Double preco) {
