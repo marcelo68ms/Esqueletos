3af6bfe94512698bf55e9cab3ca7acde2853531d
Marcelo - Dell <marcelo68ms@gmail.com>
2018-12-11 14:01:41 -0200

    Novo Projeto de Conversão e Cotação de Moedas.

--- Flutter/calculo_imc/lib/main.dart
+++ Flutter/calculo_imc/lib/main.dart
@@ -38,7 +38,7 @@ class _HomeState extends State<Home> {
       } else if (imc >= 18.6 && imc < 24.9) {
         _infoText = "Peso Ideal (${imc.toStringAsPrecision(4)})";
       } else if (imc >= 24.9 && imc < 29.9) {
-        _infoText = "Levemente Acimado peso (${imc.toStringAsPrecision(4)})";
+        _infoText = "Levemente acima do peso (${imc.toStringAsPrecision(4)})";
       } else if (imc >= 29.9 && imc < 34.9) {
         _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
       } else if (imc >= 34.9 && imc < 39.9) {
