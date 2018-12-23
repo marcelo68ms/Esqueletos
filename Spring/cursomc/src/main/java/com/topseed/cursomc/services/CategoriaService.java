d856acf7c41d7824e346e9d3b0c1467a135af5fa
Marcelo - Dell <marcelo68ms@gmail.com>
2018-12-23 19:19:52 -0200

    Desenvolvimento do serviço de Delete.

--- Spring/cursomc/src/main/java/com/topseed/cursomc/services/CategoriaService.java
+++ Spring/cursomc/src/main/java/com/topseed/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.topseed.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.topseed.cursomc.domain.Categoria;
 import com.topseed.cursomc.repositories.CategoriaRepository;
+import com.topseed.cursomc.services.exceptions.DataIntegrityException;
 import com.topseed.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return rep.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			rep.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }
