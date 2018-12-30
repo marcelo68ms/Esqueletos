e3c0755b2577ae2bfa8b9c7bef5d7195d8023ff7
Marcelo - Dell <marcelo68ms@gmail.com>
2018-12-30 09:21:29 -0200

    Implementação de validação usando o Bean Validator.

--- Spring/cursomc/src/main/java/com/topseed/cursomc/services/CategoriaService.java
+++ Spring/cursomc/src/main/java/com/topseed/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.topseed.cursomc.domain.Categoria;
+import com.topseed.cursomc.dto.CategoriaDTO;
 import com.topseed.cursomc.repositories.CategoriaRepository;
 import com.topseed.cursomc.services.exceptions.DataIntegrityException;
 import com.topseed.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return rep.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO obj) {
+		return new Categoria(obj.getId(), obj.getNome());
 	}
 }
