e6f52835ad8dc925402743a9caecbea800631736
Marcelo - Dell <marcelo68ms@gmail.com>
2018-12-31 11:58:39 -0200

    Inclusão da lista de tarefas e o tratamento dos itens da lista.

--- Flutter/lista_tarefas/lib/main.dart
+++ Flutter/lista_tarefas/lib/main.dart
@@ -19,7 +19,19 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  List _toDoList  = ["Marcelo", "Alessandra"];
+  final _toDoController = TextEditingController();
+
+  List _toDoList = [];
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo["ok"] = false;
+      _toDoList.add(newToDo);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,6 +50,7 @@ class _HomeState extends State<Home> {
               children: <Widget>[
                 Expanded(
                   child: TextField(
+                    controller: _toDoController,
                     decoration: InputDecoration(
                         labelText: "Nova tarefa",
                         labelStyle: TextStyle(color: Colors.blueAccent)
@@ -48,7 +61,7 @@ class _HomeState extends State<Home> {
                   color: Colors.blueAccent,
                   child: Text("Add"),
                   textColor: Colors.white,
-                  onPressed: () {},
+                  onPressed: _addToDo,
                 )
               ],
             ),
@@ -64,6 +77,11 @@ class _HomeState extends State<Home> {
                     secondary: CircleAvatar(
                         child: Icon(_toDoList[index]["ok"] ?
                           Icons.check : Icons.error),),
+                    onChanged: (c) {
+                      setState(() {
+                        _toDoList[index] ["ok"] = c;
+                      });
+                    },
                   );
                 }
             ),
