46fc967f5724c873f210205721d6d2695b1f3d7c
Marcelo - Dell <marcelo68ms@gmail.com>
2019-01-25 15:38:20 -0200

    Implementado a lista de tarefas

--- Flutter/lista_tarefas/lib/main.dart
+++ Flutter/lista_tarefas/lib/main.dart
@@ -51,6 +51,19 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      _toDoList.sort((a, b){
+        if (a["ok"] && !b["ok"]) return 1;
+        else if (!a["ok"] && b["ok"]) return -1;
+        else return 0;
+      });
+      _saveData();
+    });
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     // Troca o Container por Scaffold
@@ -85,11 +98,12 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-                padding: EdgeInsets.only(top:10),
-                itemCount: _toDoList.length,
-                itemBuilder: buildItem,
-            ),
+            child: RefreshIndicator(child:
+            ListView.builder(
+              padding: EdgeInsets.only(top:10),
+              itemCount: _toDoList.length,
+              itemBuilder: buildItem,
+            ), onRefresh: _refresh)
           )
         ],
       ),
