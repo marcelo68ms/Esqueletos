c952439fb1f5433131a21659f4e213688749685f
Marcelo - Dell <marcelo68ms@gmail.com>
2019-02-16 20:00:27 -0200

    Acerto do tratamento de erro de validação

---
+++ Spring/cursomc/src/main/java/com/topseed/cursomc/resources/exceptions/FieldMessage.java
@@ -0,0 +1,41 @@
+package com.topseed.cursomc.resources.exceptions;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String fielName;
+	private String message;
+	
+	public FieldMessage() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public FieldMessage(String fielName, String message) {
+		super();
+		this.fielName = fielName;
+		this.message = message;
+	}
+
+	public String getFielName() {
+		return fielName;
+	}
+
+	public void setFielName(String fielName) {
+		this.fielName = fielName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
