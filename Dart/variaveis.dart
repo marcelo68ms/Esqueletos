c9e57e690d9f2775bdf632fdf694027b5f596eed
Marcelo - Dell <marcelo68ms@gmail.com>
2019-05-01 19:45:08 -0300

    Primeira aula sobre variáveis.

---
+++ Dart/variaveis.dart
@@ -0,0 +1,16 @@
+void main() {
+  String nome = "Marcelo";
+  nome = "Santos";
+  print(nome); 
+
+  int idade = 25;
+  double altura = 1.70;
+  bool simnao = true;
+
+  if (idade < 30) {
+    print('Jovem');
+  }
+  print(idade);
+  print(altura);
+  print(simnao);
+}
