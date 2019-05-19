265eace3e4d6c9a06b896d01fcec4290e74e623d
Marcelo - Dell <marcelo68ms@gmail.com>
2019-05-19 10:40:08 -0300

    Finalização do capítulo de variáveis.

--- Dart/Inicio/hello_world/bin/main.dart
+++ Dart/Inicio/hello_world/bin/main.dart
@@ -1,5 +1,15 @@
+import 'dart:io';
 
 main(List<String> arguments) {
+  // Constantes
+  // Cria uma constante em tempo de compilação
+  const PI = 3.1415916;
+  print('O valor de PI é $PI');
+
+  // Cria uma constante em tempo de execução
+  final marcelo = new Pessoa();
+  print(marcelo);
+
   // Int - inteiro
   int pessoas = 5;
   print(pessoas);
@@ -32,4 +42,47 @@ main(List<String> arguments) {
   int anosCachorro = 7;
   int idadeCachorro = idade * anosCachorro;
   print('A sua idade em anos de cachorro é $idadeCachorro anos'); 
+
+  // Booleanos
+  bool conducao = true;
+  print('condicao = $conducao');
+
+  // Como saber o tipo de uma variável
+  print(conducao.runtimeType);
+  print(erro.runtimeType);
+
+  // String - Váriaveis Texto
+  String nome = 'Marcelo Santos';
+  print('Olá, $nome');
+
+  // Cria uma variável do tipo dinânica. Ela aceita qualquer tipo, porém uma vez atribuito o tipo
+  // o mesmo vale daí para sempre
+  var nomeDivido = nome.split(' ');
+  print('Primeiro nome: ${nomeDivido[0]}');
+  print('Segundo nome: ${nomeDivido[1]}');
+
+  print(nomeDivido.runtimeType);
+
+  String apelido = nome.substring(0,3);
+  print(apelido);
+
+  String sopa = 'Sopa de Letrinhas';
+  int index = sopa.indexOf(' de ');
+  print('A sopa é: ${sopa.substring(index).trim()}');
+  print(index);
+  print('O tamanho é de ${sopa.length}');
+
+  String presidente = "#BolsonaroNossoPresidente";
+  print('Contém hashtag: ${presidente.contains('#')}');
+
+
+  // Interações com o usuário
+  print('Qual é o seu nome ?');
+  String nomeUsuario = stdin.readLineSync();
+
+  nomeUsuario.isEmpty ? print('Nenhum nome foi informado !') : print('O nome informado foi $nomeUsuario');
+}
+
+class Pessoa {
+
 }
