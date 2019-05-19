b69c06b31fed4e485a4083971f92edd1ac49139e
Marcelo - Dell <marcelo68ms@gmail.com>
2019-05-19 11:04:43 -0300

    Incio do capítulo de Operações

---
+++ Dart/operacoes/bin/main.dart
@@ -0,0 +1,24 @@
+
+main(List<String> arguments) {
+
+  int num1 = 101;
+  int num2 = 2;
+  dynamic res = 0;
+
+  // Adição
+  res = num1 + num2;
+  print('Adição: $res');
+
+  // Subtração
+  res = num1 - num2;
+  print('Adição: $res');
+
+  // Multiplição
+  res = num1 * num2;
+  print('Adição: $res');
+
+  // Divisão
+  res = num1 / num2;
+  print('Adição: $res');
+
+}
---
+++ Dart/operacoes/test/operacoes_test.dart
@@ -0,0 +1,8 @@
+import 'package:operacoes/operacoes.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
