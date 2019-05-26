50f1cb96101559810e006c685f0648125c64f6f0
Marcelo - Dell <marcelo68ms@gmail.com>
2019-05-26 18:42:44 -0300

    Finalização do Controle de Fluxos

--- Dart/controle_fluxo/bin/main.dart
+++ Dart/controle_fluxo/bin/main.dart
@@ -22,4 +22,65 @@ main(List<String> arguments) {
     print('Você tem $idade anos e ${temDebito ? '' : 'não '}tem dívidas');
   }
 
+  // Switch e sua estrutura
+  idade = 65;
+  switch(idade) {
+    case 18:
+      print('Você pode tirar a CNH');
+      break;
+    case 21:
+      print('Você possui a maioridade');
+      break;
+    case 65:
+      print('Você pode se aposentar');
+      break;
+    default:
+      print('Nada de especial para sua idade');
+      break;
+  }
+
+  // Loops
+  // For
+  for (int i = 0; i < 10; i += 2) {
+    print('for: $i');
+  }
+
+  // For com listas
+  List letras = ['a', 'b', 'c', 'd' ];
+  for (int i = 0; i < letras.length; i ++) {
+    print('for: $i - ${letras[i]}');
+  }
+
+  // For com listas simplificado
+  for(String l in letras) {
+    print('For in: $l');
+  }
+
+  // While
+  int j = 0;
+  while(j < 10) {
+    print('while: $j');
+    j++;
+  }
+
+  // do while
+  int k = 0;
+  do {
+    print('Do while: $k');
+    k++;
+  } while(k < 10);
+
+  // for each
+  List pessoas = ['Marcelo', 'Alessandra', 'Mauro', 'Núbio', 'Margô'];
+  print(pessoas);
+  for (String l in pessoas) {
+    print(l);
+  }
+
+  // Sintaxe do For each
+  pessoas.forEach((pessoa) {
+    print(pessoa);
+  });
+
+  pessoas.forEach((pessoa) => print(pessoa));
 }
