2a967c705f9ebfe3bcbb8ef29b991274ea5c6f8e
Marcelo - Dell <marcelo68ms@gmail.com>
2019-06-02 17:52:07 -0300

    Fechamento do capítulo de imports

--- Dart/imports/bin/main.dart
+++ Dart/imports/bin/main.dart
@@ -1,3 +1,4 @@
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 main(List<String> arguments) {
@@ -6,5 +7,16 @@ main(List<String> arguments) {
   String encoded = base64.encode(eBytes);
 
   print('Encoded: $encoded');
+
+  List dBytes = base64.decode(encoded);
+  String decoded = utf8.decode(dBytes);
+
+  print('Decoded: $decoded');
+
+  var url = 'https://www.google.com';
+
+  http.get(url).then((response) {
+    print('Código de status da requisição: ${response.statusCode}');
+  });  
 
 }
