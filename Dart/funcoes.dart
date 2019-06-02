3796e890abbdaa4e4453a310b88f30908e5d8d3a
Marcelo - Dell <marcelo68ms@gmail.com>
2019-06-02 14:57:05 -0300

    Inicio dos trabalhos com funções

--- Dart/funcoes.dart
+++ Dart/funcoes.dart
@@ -1,7 +1,14 @@
+
 void main() {
   boasVindas();
   print(simulador(4.67, 16.9));
   print(calcArea(23.103));
+
+  print(centimentrosParaMetros(1500));
+  print(nomeCompleto('Marcelo', 'Santos'));
+  print(nomeCompleto('Alessandra'));
+  agora('Leka');
+  agora('Leka', true);
 }
 
 double simulador(double a, double b) {
@@ -14,3 +21,17 @@ void boasVindas() {
 }
 
 double calcArea(double raio) => 3.14 * raio * raio;
+
+double centimentrosParaMetros(num cm) {
+  return cm / 100;
+}
+
+String nomeCompleto(String primeiro, [ultimo = '']) {
+  return '$primeiro $ultimo';
+}
+
+void agora(String texto,[bool hora = false]) {
+  var agora = new DateTime.now();
+  String horaFormatada = ', a hora atual é: ${agora.hour}:${agora.minute}';
+  print('$texto, hoje é dia ${agora.day}/${agora.month}/${agora.year}${hora ? horaFormatada : ''}');
+}
