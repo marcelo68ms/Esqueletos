2032aaf04574c74f8def8855a163561c5702b25d
Marcelo - Dell <marcelo68ms@gmail.com>
2019-06-09 19:12:45 -0300

    Capítulo de Banco de dados

---
+++ Go/sql/select/select.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type usuario struct {
+	id   int
+	nome string
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@/cursogo")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	rows, _ := db.Query("select id,nome from usuarios where id > ?", 2003)
+	defer rows.Close()
+
+	for rows.Next() {
+		var u usuario
+		rows.Scan(&u.id, &u.nome)
+		fmt.Println(u)
+	}
+}
