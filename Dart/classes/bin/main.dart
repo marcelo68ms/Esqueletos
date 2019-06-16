838ef54211af4c4dfe938c884e3e0e0d5984f6a7
Marcelo - Dell <marcelo68ms@gmail.com>
2019-06-16 17:46:40 -0300

    Inicio das aulas relativas as classes

---
+++ Dart/classes/bin/main.dart
@@ -0,0 +1,18 @@
+class Pessoa {
+  String nome;
+  int idade;
+  double altura;
+
+  void aniversario() {
+    idade++;
+  }
+
+  void dorme() {
+    print('$nome está dormindo');
+  }
+}
+
+main() {
+  Pessoa pessoa1 = new Pessoa();
+   
+}
--- Dart/excecao.dart
+++ Dart/excecao.dart
@@ -1,18 +1,16 @@
 main() {
-
   int idade;
   int idadecachorro = 7;
 
   try {
     if (idadecachorro != 7)
       throw new Exception('Os anos de cachorro precisam ser 7 !');
-    if (idade == null) 
-      throw new NullThrownError();
+    if (idade == null) throw new NullThrownError();
 
     print(idade * idadecachorro);
   } on NullThrownError {
-    print('O valor está nulo');  
-  } catch(e) {
+    print('O valor está nulo');
+  } catch (e) {
     print('Ops, houve um erro com a mensagem: \n\n\n${e.toString()}');
   } finally {
     print('Pronto, executado !!');
