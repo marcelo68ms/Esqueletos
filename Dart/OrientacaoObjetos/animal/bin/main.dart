5aefb94fc52c90360551ad2ebf6e78e28d87d27b
Marcelo - Dell <marcelo68ms@gmail.com>
2019-06-21 12:19:05 -0300

    Criação e gerenciamento dos getters e setters

--- Dart/OrientacaoObjetos/animal/bin/main.dart
+++ Dart/OrientacaoObjetos/animal/bin/main.dart
@@ -3,6 +3,8 @@ import 'package:animal/animal.dart';
 main(List<String> arguments) {
   Animal gato = Animal('Meggy', 6, 'Mesclado');
   
+  gato.idade = 4;
+
   gato.dizAlo();
   gato.digaAlgo('Miau !!');
 }
--- Dart/OrientacaoObjetos/animal/lib/animal.dart
+++ Dart/OrientacaoObjetos/animal/lib/animal.dart
@@ -3,8 +3,20 @@ class Animal {
   int _idade = 0;
   String _pelo = '';
  
+ // Construtores
   Animal(this._nome, this._idade, this._pelo);
+
+  // Criar os gets e sets
+  String get nome => _nome;
+  set nome(String value) => _nome = value;
   
+  int get idade => _idade;
+  set idade(int valor) => _idade = valor;
+
+  String get pelo => _pelo
+  set pelo(String value) => _pelo = value;
+
+  // Métodos da classe
   dizAlo() => print('Olá, meu nome é $_nome e eu tenho $_idade anos, meu pêlo é $_pelo');
 
   _display(String mensagem) => print('Mensagem $mensagem ');
