cd5d86ecafee554f9d15a231d81c48c47b8c3384
Marcelo - Dell <marcelo68ms@gmail.com>
2019-06-21 16:17:37 -0300

    Exercício de Herança

--- Dart/OrientacaoObjetos/heranca/bin/main.dart
+++ Dart/OrientacaoObjetos/heranca/bin/main.dart
@@ -1,5 +1,19 @@
-import 'package:heranca/heranca.dart' as heranca;
+import 'package:heranca/cliente.dart';
 
-main(List<String> arguments) {
-  print('Hello world: ${heranca.calculate()}!');
+main() {
+  
+  print('--------------- Dados do Cliente -----------------');
+  var cliente1 = Cliente();
+  cliente1.nome = 'Marcelo Santos';
+  cliente1.cpf = '070.108.388-36';
+  cliente1.endereco = 'Rua José Revel, 197';
+
+  cliente1.entrada();
+  print('Nome: ${cliente1.nome}');
+  print('Endereço: ${cliente1.cpf}');
+  cliente1.saida();
+
+    print('--------------- Dados do Cliente -----------------');
+
+
 }
