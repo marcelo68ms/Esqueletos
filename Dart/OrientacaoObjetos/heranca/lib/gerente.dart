94326e6940ba7664ec23cf95fc4be2122d0de46f
Marcelo - Dell <marcelo68ms@gmail.com>
2019-06-21 16:46:28 -0300

    Sobreescrita de método

--- Dart/OrientacaoObjetos/heranca/lib/gerente.dart
+++ Dart/OrientacaoObjetos/heranca/lib/gerente.dart
@@ -3,4 +3,9 @@ import 'package:heranca/pessoa.dart';
 
 class Gerente extends Pessoa with Administrador {
   int tabela;
+
+  void entrada() {
+    super.entrada();
+    print('Não bate o ponto');
+  }
 }
