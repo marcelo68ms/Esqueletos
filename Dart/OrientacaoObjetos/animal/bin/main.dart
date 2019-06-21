4c6dea00ff87d32f1e23d2532f914a192af57d67
Marcelo - Dell <marcelo68ms@gmail.com>
2019-06-21 12:04:16 -0300

    Fechando a aula de acesso a classes

--- Dart/OrientacaoObjetos/animal/bin/main.dart
+++ Dart/OrientacaoObjetos/animal/bin/main.dart
@@ -3,4 +3,6 @@ import 'package:animal/animal.dart';
 main(List<String> arguments) {
   Animal gato = Animal('Meggy', 6, 'Mesclado');
   
+  gato.dizAlo();
+  gato.digaAlgo('Miau !!');
 }
--- Dart/OrientacaoObjetos/animal/lib/animal.dart
+++ Dart/OrientacaoObjetos/animal/lib/animal.dart
@@ -1,8 +1,13 @@
 class Animal {
   String _nome = '';
   int _idade = 0;
-  String pelo = '';
+  String _pelo = '';
  
-  Animal(this._nome, this._idade, this.pelo);
+  Animal(this._nome, this._idade, this._pelo);
   
+  dizAlo() => print('Olá, meu nome é $_nome e eu tenho $_idade anos, meu pêlo é $_pelo');
+
+  _display(String mensagem) => print('Mensagem $mensagem ');
+
+  digaAlgo(String mensagem) => _display(mensagem);
 }
