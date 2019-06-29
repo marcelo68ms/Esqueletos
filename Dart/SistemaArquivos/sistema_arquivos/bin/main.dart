bbc2f47ab102b947de58b6642041a6ad8f6deb9f
Marcelo - Dell <marcelo68ms@gmail.com>
2019-06-29 16:20:53 -0300

    Alta de leitura de conteúdo de arquivos

--- Dart/SistemaArquivos/sistema_arquivos/bin/main.dart
+++ Dart/SistemaArquivos/sistema_arquivos/bin/main.dart
@@ -10,7 +10,7 @@ main() {
     print('A pasta não existe !!');
   }
 
-  print('\n\n-----------------------------------\n\n');
+  print('-----------------------------------------------');
 
   // Como criar e remover um diretório no sistema de arquivos temporários
 
@@ -22,4 +22,47 @@ main() {
     print('Removendo a pasta ${dir.path}');
     dir.deleteSync();
   }
+
+  print('-----------------------------------------------');
+
+  dir = Directory.current;
+  print('Path atual: ${dir.path}');
+
+  List<FileSystemEntity> listaPastas = dir.listSync(recursive: true);
+  print('Itens na lista: ${listaPastas.length}');
+
+  // Varre todos os arquivos da lista capturada
+  listaPastas.forEach((FileSystemEntity valor) {
+  // Captura os dados dos arquivos 
+  FileStat stat = valor.statSync();
+
+  print('Path: ${valor.path}');
+  print('Tipo: ${stat.type}');
+  print('Alterado em: ${stat.changed}');
+  print('Modificado em: ${stat.modified}');
+  print('Acessado em: ${stat.accessed}');
+  print('Modo de Pemissão: ${stat.mode}');
+  print('Tamanho: ${stat.size} bytes.\n');
+
+  print('-----------------------------------------------');
+
+  // Ler dados de um arquivo texto
+  File arquivo = File(dir.path + '/meu_arquivo.txt');
+  lerArquivo(arquivo);
+  });
+}
+
+void lerArquivo(File arquivo) {
+  if (!arquivo.existsSync()) {
+    print('Aquivo não existe');
+    return;
+  }
+
+  print('Iniciando a leitura do arquivo');
+  print(arquivo.readAsStringSync());
+
+  print('Lendo os bytes ...');
+  List valores = arquivo.readAsBytesSync();
+  valores.forEach((valor) => print(valor));
+
 }
