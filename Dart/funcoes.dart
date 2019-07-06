ab838105c557ce6b72878c9d857af60932edbf97
Marcelo - Dell <marcelo68ms@gmail.com>
2019-07-06 18:49:10 -0300

    Inicio da inclusão de comentários para deixar todo o código mais claro.

--- Dart/funcoes.dart
+++ Dart/funcoes.dart
@@ -13,28 +13,35 @@ void main() {
   dadosPessoa(peso: 84, nome: 'Marcelo', altura: 1.70, idade: 50, cor: 'Pardo');
 }
 
+// Exemplo de função com retorno de um double
 double simulador(double a, double b) {
   return a*b;
 }
 
+// Função de retorno de imagem de boas vindas
 void boasVindas() {
   String nome = 'Mauro';
   print('Bem Vindo, ' + nome);
 }
 
+// Função que calcula a área de forma simplificada porque é resolvida em uma linha
 double calcArea(double raio) => 3.14 * raio * raio;
 
+// Função de conversão 
 double centimentrosParaMetros(num cm) {
   return cm / 100;
 }
 
-// Parâmetros Opcionais
+// Exemplo de função com Parâmetros Opcionais
 String nomeCompleto(String primeiro, [ultimo = '']) {
   return '$primeiro $ultimo';
 }
 
+// Função com parâmetros opcionais
 void agora(String texto,[bool hora = false]) {
+  // Captura a hora atual
   var agora = new DateTime.now();
+  // Formata a hora para um formato mais amigável
   String horaFormatada = ', a hora atual é: ${agora.hour}:${agora.minute}';
   print('$texto, hoje é dia ${agora.day}/${agora.month}/${agora.year}${hora ? horaFormatada : ''}');
 }
