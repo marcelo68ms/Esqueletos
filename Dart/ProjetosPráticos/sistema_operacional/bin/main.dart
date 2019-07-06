1a3e573f61adf47fac652ce1e5b221f32c3fe0d4
Marcelo - Dell <marcelo68ms@gmail.com>
2019-07-06 12:51:56 -0300

    Inicio do trabalho com processos

--- Dart/ProjetosPráticos/sistema_operacional/bin/main.dart
+++ Dart/ProjetosPráticos/sistema_operacional/bin/main.dart
@@ -2,6 +2,19 @@
 import 'dart:io';
 
 main() {
-  print('SO: ${Platform.operatingSystem}');
+  print('SO: ${Platform.operatingSystem} ${Platform.version}');
   
+  if (Platform.isLinux) {
+    print('Rodando Linux');
+  }
+
+  print('Path: ${Platform.script.path}');
+  print('Dart: ${Platform.executable}');
+  print('\n----------------- Variáveis de Ambiente --------------------');
+  Platform.environment.keys.forEach((key) {
+    print('${key} - ${Platform.environment[key]}');
+  });
+
+  print('\n\n------------------------- Processos ------------------------');
+    
 }
