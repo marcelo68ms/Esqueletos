54bde2281038ef4285daf6086c20f9669151b78b
Marcelo - Dell <marcelo68ms@gmail.com>
2019-07-07 13:06:05 -0300

    Inicio do módulo de Assincronismo

---
+++ Dart/OrientacaoObjetos/assincronismo/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:assincronismo/assincronismo.dart' as assincronismo;
+
+main(List<String> arguments) {
+  print('Hello world: ${assincronismo.calculate()}!');
+}
---
+++ Dart/OrientacaoObjetos/assincronismo/test/assincronismo_test.dart
@@ -0,0 +1,8 @@
+import 'package:assincronismo/assincronismo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
