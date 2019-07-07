10dd8813697747595ffa3dbd4dd3cec07d9b1e6c
Marcelo - Dell <marcelo68ms@gmail.com>
2019-07-07 17:46:15 -0300

    Efetiva a aula de criação de arquivos ZIP

--- Dart/SistemaArquivos/compressao/bin/main.dart
+++ Dart/SistemaArquivos/compressao/bin/main.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:archive/archive.dart';
+import 'package:path/path.dart' as pkpath;
 
 main() {
   String dado = '';
@@ -14,7 +16,7 @@ main() {
   List comprimida = gzip.encode(original);
 
   print(original);
-  print('-------------;----------------------------------');
+  print('------------------------------------------------');
   print(comprimida);
 
   // Dados Descompactados
@@ -28,14 +30,55 @@ main() {
   print(decoded);
   assert(dado == decoded);
 
+  // Efetua uma nova compressão e efetua uma comparativo entre zlib e gzip
   int zlib_comp = testeCompressao(zlib);
   int gzip_comp = testeCompressao(gzip);
 
   print('zlib = ${zlib_comp} bytes');
   print('gzip = ${gzip_comp} bytes');
+  print('------------------------------------------------');
 
+  List<String> arquivos = List<String>();
+  // Trabalha com diretórios a partir da localização atual
+  Directory.current.listSync().forEach((FileSystemEntity fse) {
+    if(fse.statSync().type == FileSystemEntityType.file) {
+      arquivos.add(fse.path);
+    } 
+  });
+  String zipFile = '${Directory.current.path}/comprimido.zip';
+  zip(arquivos, zipFile);
+
+  unzip(zipFile, '${Directory.current.path}/descomprimido');
 }
 
+// Funcão responsável pela criação do arquivo ZIP
+void zip(List<String> arquivos, String arquivo) {
+  Archive archive = Archive();
+  arquivos.forEach((String path){
+    File arquivo = File(path);
+    ArchiveFile archiveFile = ArchiveFile(pkpath.basename(path), arquivo.lengthSync(), arquivo.readAsBytesSync());
+    archive.addFile(archiveFile);
+  });
+
+  ZipEncoder encoder = ZipEncoder();
+  File f = File(arquivo);
+  f.writeAsBytesSync(encoder.encode(archive));
+  print('Compressão bem sucessida');
+}
+
+// Responsável por descompactar os arquivos de um ZIP para um diretório.
+void unzip(String zip, String path) {
+  File file = File(zip);
+  Archive archive = ZipDecoder().decodeBytes(file.readAsBytesSync());
+  archive.forEach((ArchiveFile archiveFile){
+    File file = File(path + '/' + archiveFile.name);
+    file.createSync(recursive: true);
+    file.writeAsBytesSync(archiveFile.content);
+  });
+  print('Arquivo Descomprimido');
+}
+
+// Gerar informações para comprimir
 String gerarDado() {
   String dado = '';
   for(int i = 0; i < 1000; i++) {
@@ -44,6 +87,7 @@ String gerarDado() {
   return dado;
 }
 
+// Efetua a compressão das informações 
 int testeCompressao(var codec) {
   String dado = gerarDado();
   
