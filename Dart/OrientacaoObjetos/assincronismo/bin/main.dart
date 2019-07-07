2283d1fb8bf322f6175f0277ffe2e2e70ca5f92f
Marcelo - Dell <marcelo68ms@gmail.com>
2019-07-07 13:47:22 -0300

    Aula sobre Future

--- Dart/OrientacaoObjetos/assincronismo/bin/main.dart
+++ Dart/OrientacaoObjetos/assincronismo/bin/main.dart
@@ -1,14 +1,31 @@
 import 'dart:async';
 
+import 'dart:io';
+
 int contator = 0;
 
 // Inicio do exemplo sobre assincronimo
 main() {
-  Duration duracao = Duration(seconds: 2);
+  Duration duracao = Duration(seconds: 2); 
+  // Função que fica aguardando um callback 
   Timer.periodic(duracao, finalTempo);
   print('Iniciado ${getTempo()}');
+  print('------------------------------------------------');
+
+  String path = Directory.current.path + '/teste.txt';
+  File arquivo = File(path);
+
+  Future<RandomAccessFile> dadoArquivo = arquivo.open(mode: FileMode.append);
+  dadoArquivo.then((RandomAccessFile raf) {
+    print('O arquivo foi Aberto !');
+    raf.writeString('Olá mundo !!').then((valor){
+      print('Arquivo escrito com sucesso !');
+    }).catchError(() => print('Ocorreu um erro !!')).whenComplete(() => raf.close());
+  });
+  print('-------------------------------------------------');
 }
 
+// Função que efetua a espera e retorna a hora atual
 void finalTempo(Timer tempo) {
   print('Espera: ${getTempo()}');
   contator ++;
@@ -17,6 +34,7 @@ void finalTempo(Timer tempo) {
   }
 }
 
+// Função retorna a hora atual
 String getTempo() {
   DateTime dt = DateTime.now();
   return dt.toString();
