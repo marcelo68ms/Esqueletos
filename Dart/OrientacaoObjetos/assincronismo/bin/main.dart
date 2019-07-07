f81655c6354685cc824f3a6dc2fde76dccd8f509
Marcelo - Dell <marcelo68ms@gmail.com>
2019-07-07 13:19:19 -0300

    Projeto de Assincronismo 1

--- Dart/OrientacaoObjetos/assincronismo/bin/main.dart
+++ Dart/OrientacaoObjetos/assincronismo/bin/main.dart
@@ -1,5 +1,23 @@
-import 'package:assincronismo/assincronismo.dart' as assincronismo;
+import 'dart:async';
 
-main(List<String> arguments) {
-  print('Hello world: ${assincronismo.calculate()}!');
+int contator = 0;
+
+// Inicio do exemplo sobre assincronimo
+main() {
+  Duration duracao = Duration(seconds: 2);
+  Timer.periodic(duracao, finalTempo);
+  print('Iniciado ${getTempo()}');
+}
+
+void finalTempo(Timer tempo) {
+  print('Espera: ${getTempo()}');
+  contator ++;
+  if (contator >= 5) {
+    tempo.cancel();
+  }
+}
+
+String getTempo() {
+  DateTime dt = DateTime.now();
+  return dt.toString();
 }
