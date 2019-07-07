10df1540c82fce18f28373f04257f039c4b1f444
Marcelo - Dell <marcelo68ms@gmail.com>
2019-07-07 16:33:51 -0300

    Final da aula sobre gzip

--- Dart/SistemaArquivos/compressao/bin/main.dart
+++ Dart/SistemaArquivos/compressao/bin/main.dart
@@ -1,4 +1,30 @@
+import 'dart:convert';
+import 'dart:io';
 
 main() {
- 
+  String dado = '';
+  for(int i = 0; i < 100; i++) {
+    dado = dado + 'Olá mundo \r\n';
+  }
+
+  // Dados originais
+  List original = utf8.encode(dado);
+
+  // Dados comprimidos
+  List comprimida = gzip.encode(original);
+
+  print(original);
+  print('-------------;----------------------------------');
+  print(comprimida);
+
+  // Dados Descompactados
+  List descompactado = gzip.decode(comprimida);
+
+  print('Original ${original.length} bytes');
+  print('Comprimido ${comprimida.length} bytes');
+  print('Descomprimido ${descompactado.length} bytes');
+
+  String decoded = utf8.decode(descompactado);
+  print(decoded);
+  assert(dado == decoded);
 }
