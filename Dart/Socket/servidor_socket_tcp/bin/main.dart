1ae64888f2fb056bf4892b1ae15bf20360ded11c
Marcelo - Dell <marcelo68ms@gmail.com>
2019-07-08 12:08:56 -0300

    Inicio da seção de Socket

---
+++ Dart/Socket/servidor_socket_tcp/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:servidor_socket_tcp/servidor_socket_tcp.dart' as servidor_socket_tcp;
+
+main(List<String> arguments) {
+  print('Hello world: ${servidor_socket_tcp.calculate()}!');
+}
---
+++ Dart/Socket/servidor_socket_tcp/test/servidor_socket_tcp_test.dart
@@ -0,0 +1,8 @@
+import 'package:servidor_socket_tcp/servidor_socket_tcp.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
