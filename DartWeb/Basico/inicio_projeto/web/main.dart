b29da1b05dc8fd1a8fb92092992e43acac75d168
Marcelo - Dell <marcelo68ms@gmail.com>
2019-07-09 18:02:58 -0300

    Inclusão de uma captura de clique.

--- DartWeb/Basico/inicio_projeto/web/main.dart
+++ DartWeb/Basico/inicio_projeto/web/main.dart
@@ -2,4 +2,9 @@ import 'dart:html';
 
 void main() {
   querySelector('#output').text = 'Your Dart app is running.';
+
+  querySelector('#output').onClick.listen((a){
+    querySelector('#output').text = 'Hello Word';  
+    print('Houve um clique');
+  });
 }
