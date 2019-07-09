0d653a6883bbddde94c82216c5358ee6cc80b1dd
Marcelo - Dell <marcelo68ms@gmail.com>
2019-07-09 11:12:49 -0300

    Inicio da sessão de banco de dados

---
+++ Dart/ProjetosPráticos/bdproj1/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:bdproj1/bdproj1.dart' as bdproj1;
+
+main(List<String> arguments) {
+  print('Hello world: ${bdproj1.calculate()}!');
+}
---
+++ Dart/ProjetosPráticos/bdproj1/test/bdproj1_test.dart
@@ -0,0 +1,8 @@
+import 'package:bdproj1/bdproj1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
