a5510c909256037a87575d0f186b72bed79c67b6
Marcelo - Dell <marcelo68ms@gmail.com>
2019-07-09 12:30:19 -0300

    Atualização dos drivers do Dart

--- Dart/ProjetosPráticos/bdproj1/bin/main.dart
+++ Dart/ProjetosPráticos/bdproj1/bin/main.dart
@@ -1,5 +1,28 @@
 import 'package:bdproj1/bd.dart' as bd;
+import 'package:mysql1/mysql1.dart';
 
-main() {
+main() async {
+  var conn = await bd.createConnecton();
 
+  await createTable(conn);
+  await insertData(conn);
+  await conn.close();
+}
+
+Future<void> createTable(MySqlConnection conn) async {
+  print('Criando Tabela ...');
+  await conn.query('CREATE TABLE IF NOT EXISTS pessoa (id INTEGER NOT NULL auto_increment, nome VARCHAR(150), idade INTEGER, email VARCHAR(200), PRIMARY KEY(id))');
+  print('Tabela criada com sucesso');
+}
+
+Future<void> insertData(MySqlConnection conn) async {
+  print('Inserindo informações');
+  var data = [
+    ['Alessandra Paula', 'mulhereacessorios@gmail.com', 43],
+    ['Marcelo Santos', 'marcelo68ms@gmail.com', 50],
+    ['MauroAlexandre', 'mauro06aps@gmail.com', 43]
+  ];
+
+  await conn.queryMulti('INSERT INTO pessoa (NOME, EMAIL, IDADE) VALUES(?, ?, ?)', data);
+  print('Dados inseridos com Sucesso !!');
 }
--- Dart/ProjetosPráticos/bdproj1/lib/bd.dart
+++ Dart/ProjetosPráticos/bdproj1/lib/bd.dart
@@ -1,11 +1,14 @@
-import 'package:sqljocky5/sqljocky.dart';
+import 'package:mysql1/mysql1.dart';
 
-createConnecton() {
-  return ConnectionSettings(
+createConnecton() async {
+  
+  var stringConexao = ConnectionSettings(
     user: "root",
     password: "root",
     host: "localhost",
     port: 3306,
     db: "dbDart",
   );
+
+  return await MySqlConnection.connect(stringConexao);
 }
