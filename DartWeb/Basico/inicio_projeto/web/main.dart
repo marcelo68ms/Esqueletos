9b092ace1c68688b8e559f51c23cb73bb157970a
Marcelo - Dell <marcelo68ms@gmail.com>
2019-07-20 13:32:46 -0300

    Trabalho com a aplicação de busca de logradouros

--- DartWeb/Basico/inicio_projeto/web/main.dart
+++ DartWeb/Basico/inicio_projeto/web/main.dart
@@ -1,10 +1,20 @@
+import 'dart:convert';
 import 'dart:html';
 
-void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+import 'package:http/http.dart' as http;
 
-  querySelector('#output').onClick.listen((a){
-    querySelector('#output').text = 'Hello Word';  
-    print('Houve um clique');
+void main() {
+  // Monitorar o click do botão da página
+  querySelector('#busca').onClick.listen((a) async {
+
+    // Captura o valor digitado no campo CEP
+    String cep = (querySelector('#cep') as InputElement).value;
+
+    // Efetua o consumo do servivo Rest 
+    String url = 'http://viacep.com.br/ws/$cep/json/';
+    var response = await http.get(url);
+    var body = json.decode(response.body);
+    print(body);
+
   });
 }
