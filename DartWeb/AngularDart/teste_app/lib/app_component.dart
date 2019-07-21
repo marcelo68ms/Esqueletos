e1a9f5803fcc932425250e37737e5c0e4a506794
Marcelo - Dell <marcelo68ms@gmail.com>
2019-07-21 12:45:22 -0300

    Arrumando para incluir o Hello World

--- DartWeb/AngularDart/teste_app/lib/app_component.dart
+++ DartWeb/AngularDart/teste_app/lib/app_component.dart
@@ -1,15 +1,11 @@
 import 'package:angular/angular.dart';
 
-import 'src/todo_list/todo_list_component.dart';
-
-// AngularDart info: https://webdev.dartlang.org/angular
-// Components info: https://webdev.dartlang.org/components
 
 @Component(
   selector: 'my-app',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [TodoListComponent],
+  directives: [],
 )
 class AppComponent {
   // Nothing here yet. All logic is in TodoListComponent.
