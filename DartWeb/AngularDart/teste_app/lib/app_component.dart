55fadb478da8206927bd1d4d6f8b4c78854a8125
Marcelo - Dell <marcelo68ms@gmail.com>
2019-07-21 13:08:06 -0300

    Finalização do Hello World

--- DartWeb/AngularDart/teste_app/lib/app_component.dart
+++ DartWeb/AngularDart/teste_app/lib/app_component.dart
@@ -1,11 +1,11 @@
 import 'package:angular/angular.dart';
-
+import 'src/hello_world/hello_world_component.dart';
 
 @Component(
   selector: 'my-app',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [],
+  directives: [HelloWorldComponent],
 )
 class AppComponent {
   // Nothing here yet. All logic is in TodoListComponent.
--- DartWeb/AngularDart/teste_app/lib/src/hello_world/hello_world_component.dart
+++ DartWeb/AngularDart/teste_app/lib/src/hello_world/hello_world_component.dart
@@ -1,7 +1,7 @@
 import 'package:angular/angular.dart';
 
 @Component(
-  selector: 'hello-word',
+  selector: 'hello-world',
   styleUrls: ['hello_world_component.css'],
   templateUrl: 'hello_world_component.html',
   directives: [],
