3b6ac0bf574b6eae7b294349d85ed08cb06894bb
Marcelo - Dell <marcelo68ms@gmail.com>
2019-07-21 15:10:35 -0300

    Inclusão da Injeção de dependências e de serviços

--- DartWeb/AngularDart/teste_app/lib/src/list/list_component.dart
+++ DartWeb/AngularDart/teste_app/lib/src/list/list_component.dart
@@ -1,12 +1,22 @@
 import 'package:angular/angular.dart';
+import 'list_service.dart';
 
 @Component(
   selector: 'list',
   styleUrls: ['list_component.css'],
   templateUrl: 'list_component.html',
   directives: [NgFor],
+  providers: [ClassProvider(ListService)]
 ) 
 
-class ListComponent {
-  List linguagens = ['Dart', 'Java', 'C', 'Cobol', 'Go Lang'];
+class ListComponent implements OnInit {
+  // Injeção de Dependencia
+  final ListService listService;
+  ListComponent(this.listService);
+
+  List linguagens = [];
+  
+  void ngOnInit() {
+    linguagens = listService.getLinguagens();
+  }
 }
