b52c02be4ea7621be1b4b85d7d7c68eb294e5075
Marcelo - Dell <marcelo68ms@gmail.com>
2019-07-21 13:02:26 -0300

    Reescrita dos componentes

---
+++ DartWeb/AngularDart/teste_app/lib/src/hello_world/hello_world_component.dart
@@ -0,0 +1,12 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'hello-word',
+  styleUrls: ['hello_world_component.css'],
+  templateUrl: 'hello_world_component.html',
+  directives: [],
+)
+
+class HelloWorldComponent {
+  
+}
