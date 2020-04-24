a761989b0d1c8004a28108a41e2dc3316747eff7
Marcelo - Dell <marcelo68ms@gmail.com>
2020-04-24 16:47:54 -0300

    Evolução no CRUD de Clientes.

--- Spring/cursomc/src/main/java/com/topseed/cursomc/resources/ClienteResource.java
+++ Spring/cursomc/src/main/java/com/topseed/cursomc/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package com.topseed.cursomc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.topseed.cursomc.domain.Cliente;
+import com.topseed.cursomc.dto.ClienteDTO;
 import com.topseed.cursomc.services.ClienteService;
 
 /**
@@ -33,5 +42,64 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	/**
+	 * Método que efetiva a alteração de informações da Cliente específica
+	 * @param objDTO
+	 * @param id
+	 * @return mensagem de HTTP de retorno
+	 */
+	@RequestMapping(value="/{id}",method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Integer id) {
+		Cliente obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	/**
+	 * Método responsável por apagar uma Cliente específica 
+	 *
+	 * @param id
+	 * @return mensagem de HTTP de retorno
+	 */
+	@RequestMapping(value="/{id}",method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();		
+	}
+	
+	/**
+	 * Efetua a listagem de todas as Clientes
+	 * 
+	 * @return uma lista de Clientes
+	 */
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> listObj = service.findAll();
+		List<ClienteDTO> listDTO = listObj.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDTO);
+	}
+
+	/**
+	 * Lista todas as Clientes de forma paginada 
+	 * 
+	 * @param page
+	 * @param linesPerPage
+	 * @param orderBy
+	 * @param direction
+	 * @return
+	 */
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Cliente> listObj = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDTO = listObj.map(obj -> new ClienteDTO(obj));
+		
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
--- Spring/cursomc/src/main/java/com/topseed/cursomc/services/ClienteService.java
+++ Spring/cursomc/src/main/java/com/topseed/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.topseed.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.topseed.cursomc.domain.Cliente;
+import com.topseed.cursomc.dto.ClienteDTO;
 import com.topseed.cursomc.repositories.ClienteRepository;
+import com.topseed.cursomc.services.exceptions.DataIntegrityException;
 import com.topseed.cursomc.services.exceptions.ObjectNotFoundException;
 
 /**
@@ -32,4 +39,69 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Nome: " + Cliente.class.getName()));
  	}
+	
+	/**
+	 * Atualiza um Cliente no banco de dados
+	 * 
+	 * @param obj
+	 * @return
+	 */
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return rep.save(newObj);
+	}
+	
+	/**
+	 * Apaga um Cliente de uma ID específica
+	 * 
+	 * @param id
+	 */
+	public void delete(Integer id) {
+		find(id);
+		try {
+			rep.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um Cliente porque possui entidades relacionadas.");
+		}
+	}
+	
+	/**
+	 * Efetua a busca de Clientes, retornando tudo numa lista geral
+	 * 
+	 * @return
+	 */
+	public List<Cliente> findAll(){
+		return rep.findAll();
+	}
+	
+	/**
+	 * Efetua uma busca de Clientes paginadas   
+	 *
+	 * @param page
+	 * @param linesPerPage
+	 * @param orderBy
+	 * @param direction
+	 * @return
+	 */
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return rep.findAll(pageRequest);
+	}
+	
+	/**
+	 * Transforma o DTO em Cliente
+	 * 
+	 * @param obj
+	 * @return
+	 */
+	public Cliente fromDTO(ClienteDTO obj) {
+		return new Cliente(obj.getId(), obj.getNome(), obj.getEmail(), null, null);
+	}
+	
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 }
