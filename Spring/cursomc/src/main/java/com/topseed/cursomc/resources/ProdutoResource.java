7280a7805d6fab4d46705304be4c87e0d51722cc
Marcelo - Dell <marcelo68ms@gmail.com>
2020-05-08 18:38:05 -0300

    Aula 50

---
+++ Spring/cursomc/src/main/java/com/topseed/cursomc/resources/ProdutoResource.java
@@ -0,0 +1,65 @@
+package com.topseed.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.topseed.cursomc.domain.Categoria;
+import com.topseed.cursomc.domain.Produto;
+import com.topseed.cursomc.dto.CategoriaDTO;
+import com.topseed.cursomc.dto.ProdutoDTO;
+import com.topseed.cursomc.services.ProdutoService;
+
+/**
+ * Classe responsável pelo controle do serviço de Produtos
+ * 
+ * @author marcelo
+ *
+ */
+@RestController
+@RequestMapping(value="/produtos")
+public class ProdutoResource {
+	
+	@Autowired
+	ProdutoService service;
+	
+	/**
+	 * Busca de Produtos por um ID específico
+	 * 
+	 * @param id
+	 * @return mensagem de HTTP de retorno
+	 */
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<Produto> find(@PathVariable Integer id) {
+		Produto obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	/**
+	 * Lista todos os Produtos de forma paginada 
+	 * 
+	 * @param page
+	 * @param linesPerPage
+	 * @param orderBy
+	 * @param direction
+	 * @return
+	 */
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<Page<ProdutoDTO>> findPage(
+			@RequestParam(value="nome", defaultValue="") String nome,
+			@RequestParam(value="categorias", defaultValue="") Integer categorias,
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Produto> listObj = service.search(nome,???, page, linesPerPage, orderBy, direction);
+		Page<ProdutoDTO> listDTO = listObj.map(obj -> new CategoriaDTO(obj));
+		
+		return ResponseEntity.ok().body(listDTO);
+	}
+}
--- Spring/cursomc/src/main/java/com/topseed/cursomc/services/ProdutoService.java
+++ Spring/cursomc/src/main/java/com/topseed/cursomc/services/ProdutoService.java
@@ -1,11 +1,19 @@
 package com.topseed.cursomc.services;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.topseed.cursomc.domain.Categoria;
 import com.topseed.cursomc.domain.Produto;
+import com.topseed.cursomc.repositories.CategoriaRepository;
+import com.topseed.cursomc.repositories.ProdutoRepository;
+import com.topseed.cursomc.services.exceptions.ObjectNotFoundException;
 
 /**
  * ProdutoService
@@ -17,8 +25,29 @@ import com.topseed.cursomc.domain.Produto;
 @Service
 public class ProdutoService {
 
-	public Page<Produto> search(String nome, List<Integer> ids) {
+	@Autowired
+	private ProdutoRepository rep;
+	
+	@Autowired
+	private CategoriaRepository repCat;
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		
-		return null;
+		List<Categoria> categorias = repCat.findAllById(ids);
+		
+		return rep.search(nome, categorias, pageRequest);
 	}
+	
+	/**
+	 * Busca de um único Produto, baseado no seu ID
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public Produto find(Integer id) {
+		Optional<Produto> obj = rep.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Produto: " + Produto.class.getName()));
+ 	}
 }
