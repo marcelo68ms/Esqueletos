160d61ac8e5ad9dbf016d94645576256d32501e5
Marcelo - Dell <marcelo68ms@gmail.com>
2020-05-29 15:47:13 -0300

    Aula 69 - Completa

--- Spring/cursomc/src/main/java/com/topseed/cursomc/services/ClienteService.java
+++ Spring/cursomc/src/main/java/com/topseed/cursomc/services/ClienteService.java
@@ -8,6 +8,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,6 +32,9 @@ import com.topseed.cursomc.services.exceptions.ObjectNotFoundException;
 @Service
 public class ClienteService {
 
+	@Autowired
+	private BCryptPasswordEncoder pe;
+	
 	@Autowired
 	private ClienteRepository rep;
 	
@@ -120,7 +124,7 @@ public class ClienteService {
 	 * @return
 	 */
 	public Cliente fromDTO(ClienteDTO obj) {
-		return new Cliente(obj.getId(), obj.getNome(), obj.getEmail(), null, null);
+		return new Cliente(obj.getId(), obj.getNome(), obj.getEmail(), null, null, null);
 	}
 
 	/**
@@ -130,7 +134,7 @@ public class ClienteService {
 	 * @return
 	 */
 	public Cliente fromDTO(ClienteNewDTO obj) {
-		Cliente cli = new Cliente(null, obj.getNome(), obj.getEmail(), obj.getCpfOuCnpj(), TipoCliente.toEnum(obj.getTipo()));
+		Cliente cli = new Cliente(null, obj.getNome(), obj.getEmail(), obj.getCpfOuCnpj(), TipoCliente.toEnum(obj.getTipo()), pe.encode(obj.getSenha()) );
 		Cidade cid = new Cidade(obj.getCidadeId(), null, null);
 		Endereco end = new Endereco(null, obj.getLogradouro(), obj.getNumero(), obj.getComplemento(), obj.getBairro(), obj.getCep(), cli, cid);		
 		cli.getEnderecos().add(end);
--- Spring/cursomc/src/main/java/com/topseed/cursomc/services/DBService.java
+++ Spring/cursomc/src/main/java/com/topseed/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.topseed.cursomc.domain.Categoria;
@@ -33,6 +34,9 @@ import com.topseed.cursomc.repositories.ProdutoRepository;
 @Service
 public class DBService {
 
+	@Autowired
+	private BCryptPasswordEncoder pe;
+	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
@@ -101,7 +105,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "marcelo68ms@yahoo.com.br", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "marcelo68ms@yahoo.com.br", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123456") );
 		cli1.getTelefones().addAll(Arrays.asList("71626381","87817946"));		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303","Jardim", "343456009", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800","Centro", "11070101", cli1, c2);		
