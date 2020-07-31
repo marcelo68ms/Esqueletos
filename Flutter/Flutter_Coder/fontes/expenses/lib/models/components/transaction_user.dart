b24f95f87c962a914d771acf5f85427d5943c451
Marcelo - Dell <marcelo68ms@gmail.com>
2020-07-31 12:57:36 -0300

    Aula 95 finalizada

--- Flutter/Flutter_Coder/fontes/expenses/lib/models/components/transaction_user.dart
+++ Flutter/Flutter_Coder/fontes/expenses/lib/models/components/transaction_user.dart
@@ -30,6 +30,24 @@ class _TransactionUserState extends State<TransactionUser> {
       value: 89.63,
       date: DateTime.now(),
     ),
+    Transaction(
+      id: 't4',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't5',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't6',
+      title: 'Conta de Água',
+      value: 89.63,
+      date: DateTime.now(),
+    ),
   ];
 
   _addTransaction(String title, double value) {
