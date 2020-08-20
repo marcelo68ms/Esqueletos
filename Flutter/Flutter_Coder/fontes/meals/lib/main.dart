21d882c03bda1398c027ef2e783c676f38b25e3a
Marcelo - Dell <marcelo68ms@gmail.com>
2020-08-20 13:31:19 -0300

    Aula 175 Finalizada

--- Flutter/Flutter_Coder/fontes/meals/lib/main.dart
+++ Flutter/Flutter_Coder/fontes/meals/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/categories_screens.dart';
+import 'screens/categories_meals_screen.dart';
+import 'Utils/app_routes.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,7 +22,10 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      home: CategoriesScreen(),
+      routes: {
+        AppRoutes.HOME: (ctx) => CategoriesScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+      },
     );
   }
 }
