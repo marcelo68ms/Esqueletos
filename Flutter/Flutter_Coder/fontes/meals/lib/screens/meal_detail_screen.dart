137ee840598751195d49b42b98d0b3a6215a9d30
Marcelo - Dell <marcelo68ms@gmail.com>
2020-09-25 12:20:04 -0300

    Aula 190 Finalizada

--- Flutter/Flutter_Coder/fontes/meals/lib/screens/meal_detail_screen.dart
+++ Flutter/Flutter_Coder/fontes/meals/lib/screens/meal_detail_screen.dart
@@ -86,6 +86,12 @@ class MealDetailScreen extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pop(meal.title);
+        },
+        child: Icon(Icons.star),
+      ),
     );
   }
 }
--- Flutter/Flutter_Coder/fontes/meals/lib/screens/settings_screen.dart
+++ Flutter/Flutter_Coder/fontes/meals/lib/screens/settings_screen.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/main_drawer.dart';
+import 'package:meals/models/settings.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  var settings = Settings();
+
+  Widget _createSwitch(
+    String title,
+    String subtitle,
+    bool value,
+    Function onChanged,
+  ) {
+    return SwitchListTile.adaptive(
+      value: value,
+      onChanged: onChanged,
+      title: Text(title),
+      subtitle: Text(subtitle),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,8 +31,47 @@ class SettingsScreen extends StatelessWidget {
         title: Text('Configurações'),
         actions: [],
       ),
-      body: Center(
-        child: Text('Configurações'),
+      drawer: MainDrawer(),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Configurações',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                _createSwitch(
+                  'Sem Glutén',
+                  'Só exibe refeições sem glúten !',
+                  settings.isGlutenFree,
+                  (value) => setState(() => settings.isGlutenFree = value),
+                ),
+                _createSwitch(
+                  'Sem Lactose',
+                  'Só exibe refeições sem lactose !',
+                  settings.isLactoseFree,
+                  (value) => setState(() => settings.isLactoseFree = value),
+                ),
+                _createSwitch(
+                  'Vegano',
+                  'Só exibe refeições veganas !',
+                  settings.isVegan,
+                  (value) => setState(() => settings.isVegan = value),
+                ),
+                _createSwitch(
+                  'Vegetarianas',
+                  'Só exibe refeições vegetarianas !',
+                  settings.isVegetarian,
+                  (value) => setState(() => settings.isVegetarian = value),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
