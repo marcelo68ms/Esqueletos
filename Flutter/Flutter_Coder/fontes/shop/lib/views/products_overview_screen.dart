4717578a708bac3cc8cfdb881664a939a21f0b42
Marcelo - Dell <marcelo68ms@gmail.com>
2020-10-02 16:45:25 -0300

    Aula 215 finalizada

--- Flutter/Flutter_Coder/fontes/shop/lib/views/products_overview_screen.dart
+++ Flutter/Flutter_Coder/fontes/shop/lib/views/products_overview_screen.dart
@@ -1,30 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shop/providers/products.dart';
-import 'package:shop/widgets/product_item.dart';
+import 'package:shop/widgets/product_grid.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final productsProvider = Provider.of<Products>(context);
-    final products = productsProvider.items;
-
     return Scaffold(
       appBar: AppBar(
         actions: [],
         title: Text('Minha Loja'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: products.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemBuilder: (ctx, i) => ProductItem(products[i]),
-      ),
+      body: ProductGrid(),
     );
   }
 }
