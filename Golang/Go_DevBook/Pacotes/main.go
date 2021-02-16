38b37a95b7cc1a1c3978294bc90c13d7313d77da
Marcelo Santos <marcelo68ms@gmail.com>
2021-02-16 12:19:58 -0300

    Aula 15 de GoLang

---
+++ Golang/Go_DevBook/Pacotes/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
+)
+
+func main() {
+
+	fmt.Println("Escrevendo a partir do main")
+	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("devbook@gmail.com")
+
+	fmt.Println(erro)
+}
