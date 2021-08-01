7de6179b4ea121dbe5347ab394d6c7ae1b3f51b5
Marcelo Santos <marcelo68ms@gmail.com>
2021-07-31 21:15:14 -0300

    Primeiro aplicativo sobre crypto

---
+++ Flutter/Youtube/flutter_application_1/lib/pages/moedas_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MoedasPage extends StatelessWidget {
+  const MoedasPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Crypto Moedas'),
+      ),
+      body: Center(
+        child: Text('Olá Mundo'),
+      ),
+    );
+  }
+}
