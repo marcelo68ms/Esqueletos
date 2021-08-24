b255b594c890d366f42264475bc0608c9c3c3ca6
Marcelo Santos <marcelo68ms@gmail.com>
2021-08-24 18:05:09 -0300

    Evolução da Aula do Youtube agora com o banco Hive

--- Flutter/Youtube/flutter_application_1/lib/main.dart
+++ Flutter/Youtube/flutter_application_1/lib/main.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/config/app_settings.dart';
 import 'package:flutter_application_1/repository/favoritas_repository.dart';
 import 'package:provider/provider.dart';
+import 'config/hive_config.dart';
 import 'meu_aplicativo.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await HiveConfig.start();
+
   runApp(
     MultiProvider(
       providers: [
--- Flutter/Youtube/flutter_application_1/lib/pages/moedas_page.dart
+++ Flutter/Youtube/flutter_application_1/lib/pages/moedas_page.dart
@@ -125,7 +125,8 @@ class _MoedasPageState extends State<MoedasPage> {
                       fontWeight: FontWeight.w500,
                     ),
                   ),
-                  if (favoritas.lista.contains(tabela[moeda]))
+                  if (favoritas.lista
+                      .any((fav) => fav.sigla == tabela[moeda].sigla))
                     Icon(
                       Icons.circle,
                       color: Colors.amber,
