e782f78f31c48429c195bb4470e0d0754fb2af5d
Marcelo M Santos <marcelo68ms@gmail.com>
2023-04-17 09:13:32 -0300

    Alteração de Segurança pedida pelo GotHub

--- Flutter/Flutter_Daniel/conversor_moeda/conversor_moeda/ios/Flutter/flutter_export_environment.sh
+++ Flutter/Flutter_Daniel/conversor_moeda/conversor_moeda/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/marcelo/JavaSoft/Mobile/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/marcelo/Trabalho/Empresas/Esqueletos/Flutter/Flutter_Daniel/conversor_moeda/conversor_moeda"
+export "FLUTTER_ROOT=/home/marcelo/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/marcelo/Projetos/Esqueletos/Flutter/Flutter_Daniel/conversor_moeda/conversor_moeda"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
